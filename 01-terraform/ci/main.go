b5babec042108bdaea8cde1d2f222642587a1ca4
Cornelius Schuchardt <cornelius.schuchardt@gmail.com>
2023-08-22 08:16:53 +0000

    add tf plan pipeline

--- 01-terraform/ci/main.go
+++ 01-terraform/ci/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"dagger.io/dagger"
@@ -20,11 +21,11 @@ func build(ctx context.Context) error {
 	fmt.Println("Terraform Plan Pipeline")
 
 	vars := []string{"AZURE_SUBSCRIPTION_ID", "AZURE_TENANT_ID", "AZURE_CLIENT_ID", "AZURE_CLIENT_SECRET"}
-    for _, v := range vars {
-        if os.Getenv(v) == "" {
-            log.Fatalf("Environment variable %s is not set", v)
-        }
-    }
+	for _, v := range vars {
+		if os.Getenv(v) == "" {
+			log.Fatalf("Environment variable %s is not set", v)
+		}
+	}
 
 	// initialize Dagger client
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
@@ -33,35 +34,51 @@ func build(ctx context.Context) error {
 	}
 	defer client.Close()
 
+	// Get Current Working Directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Working Directory could not be found")
+	}
 	// get reference to the local project
-    src := client.Host().Directory("/home/decline/dagger-demo/01-terraform")
+	src := client.Host().Directory(filepath.Dir(cwd))
 
-    // get `terraform` image
-    terraform := client.Container().From("hashicorp/terraform:latest")
+	// get `terraform` image
+	terraform := client.Container().From("hashicorp/terraform:latest")
 
-    // mount cloned repository into `terraform` image
-    terraform = terraform.WithDirectory("/src", src).WithWorkdir("/src")
+	// mount cloned repository into `terraform` image
+	terraform = terraform.WithDirectory("/src", src).WithWorkdir("/src")
+
+	// create output folder on runner
+	planPath := cwd + "/plan"
+	err = os.Mkdir(planPath, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Output folder %s could not be create", planPath)
+	}
+
+	// Mount Plan folder to container
+	planDirectory := client.Host().Directory(planPath)
+	terraform = terraform.WithDirectory("/plan", planDirectory)
 
 	// set Environment Variables in Container
-	for _,v := range vars {
-		terraform = terraform.WithEnvVariable(strings.Replace(v,"AZURE","ARM",1), os.Getenv(v))
+	for _, v := range vars {
+		terraform = terraform.WithEnvVariable(strings.Replace(v, "AZURE", "ARM", 1), os.Getenv(v))
 	}
 
 	// define the Terraform Init Command
-    terraform = terraform.WithExec([]string{"init"})
+	terraform = terraform.WithExec([]string{"init"})
 
 	// define the Terraform Plan Command
-	path := "/src"
-    terraform = terraform.WithExec([]string{"plan","-out","tfplan"})
+	path := "/plan/tfplan"
+	terraform = terraform.WithExec([]string{"plan", "-out", path})
 
-    // get reference to build output directory in container
-    output := terraform.Directory(path)
+	// get reference to build output directory in container
+	output := terraform.Directory(filepath.Dir(path))
 
-    // write contents of container build/ directory to the host
-    _, err = output.Export(ctx, path)
-    if err != nil {
-        return err
-    }
+	// write contents of container build/ directory to the host
+	_, err = output.Export(ctx, planPath)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
